4ee8bd58ee5b4cc2c0325361fd5b270993794ebb
Tyler Nguyen <tylernguyen360@yahoo.com>
2025-01-20 21:36:49 -0800

    Inserted Megaman sprite and create megaman file

---
+++ Empty--GameEngine-main/Empty--GameEngine-main/megaman.js
@@ -0,0 +1,14 @@
+class MegaMan {
+
+    constructor() {
+
+    };
+
+    update() {
+
+    };
+
+    draw(ctx) {
+        ctx.drawImage(ASSET_MANAGER.getAsset("./Megaman sprite.png"), 0, 0);
+    };
+}
